c78e2712527faf6930873acdd5455375d9bb6506
Miyamura <felipesilsou1101@gmail.com>
2025-03-02 06:28:10 -0300

    Exercicio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartfone Nokia: ");
+Smartphone Nokai = new Nokia(numero: "1101", modelo: "Modelo 1", imei: "1111111111", memoria: 64);
+Nokai.Ligar();
+Nokai.ReceberLigacao();
+Nokai.InstalarAplicativo("Genshin");
+
+Console.WriteLine("\n----------------------------\n"); 
+
+Console.WriteLine("Smartfomne Iphone");
+Smartphone Iphone = new Iphone(numero: "0213", modelo: "Modelo 2", imei: "2222222222", memoria: 128);
+Iphone.Ligar();
+Iphone.ReceberLigacao();
+Iphone.InstalarAplicativo("Pokemon Go");
